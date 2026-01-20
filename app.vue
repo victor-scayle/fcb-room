484a0c5335d348bcb62f5cbb6e0204f8ffb9e992
victor-scayle <victor.coltisor@scayle.com>
2026-01-20 17:48:36 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Keyboard, Pagination, Navigation } from 'swiper/modules';
+
+const slides = [
+  'https://img.fcbayern.com/image/upload/f_auto,q_auto,w_1280/eCommerce/produkte/53822_3',
+  'https://img.fcbayern.com/image/upload/f_auto,q_auto,w_1280/eCommerce/produkte/53822',
+  'https://img.fcbayern.com/image/upload/f_auto,q_auto,w_1280/eCommerce/produkte/53822_1',
+  'https://img.fcbayern.com/image/upload/f_auto,q_auto,w_1280/eCommerce/produkte/53822_2',
+];
+</script>
+
+<template>
+  <main>
+    <h2>Gallery</h2>
+    <p>
+      Open this project on your phone, and notice how we cannot swipe between
+      slides or scroll the page from within the Slider/3D preview.
+    </p>
+
+    <Swiper
+      :keyboard="true"
+      :css-mode="true"
+      :lazy-preload-prev-next="2"
+      :pagination="{ enabled: true, clickable: true }"
+      :modules="[Pagination, Keyboard]"
+    >
+      <SwiperSlide>
+        <iframe
+          src="https://viewer.rooom.com/product/6e4b03fd05271278164ced0680b777?autostart=1"
+          allowfullscreen
+          :mozallowfullscreen="true"
+          :webkitallowfullscreen="true"
+        ></iframe>
+      </SwiperSlide>
+
+      <SwiperSlide v-for="slide in slides" :key="`slide-${slide}`">
+        <img :src="slide" />
+      </SwiperSlide>
+    </Swiper>
+  </main>
+</template>
+
+<style lang="scss">
+iframe {
+  width: 100%;
+  height: 500px;
+  border: none;
+}
+img {
+  width: auto;
+  height: 500px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,4 @@
+export default defineNuxtConfig({
+  modules: ['nuxt-swiper'],
+  ssr: false,
+})
